a0106e388175563847eb482057ce484c5914dfe7
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-04 18:44:42 +0200

    Add serialization

--- lib/essay-paranoia.rb
+++ lib/essay-paranoia.rb
@@ -5,11 +5,19 @@ module Essay
     def paranoid?
       model_class.respond_to?(:paranoia_column)
     end
+
+    serialize do
+      { acts_as_paranoid: paranoid? }
+    end
   end
 
   class AttributeRoles
     def paranoia_timestamp?
       model_class.try(:paranoia_column).try(:to_s) == attribute_name.to_s
+    end
+
+    serialize do
+      { is_paranoia_timestamp: paranoia_timestamp? }
     end
   end
 end
